c959f30dd7d1d60d167277064369cafbc3cd6fe0
benhautmann <benbela@web.de>
2023-11-08 13:05:32 +0100

    Bottom Navigator finished 109 - more complex then last commit, with tabBatActiveTintColor

--- Navigation.jsx
+++ Navigation.jsx
@@ -4,32 +4,42 @@ import HomeScreen from './screens/HomeScreen';
 import SettingsScreen from './screens/SettingsScreen';
 import * as Icon from "@expo/vector-icons"
 
-const Tab1 = createBottomTabNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function Navigation() {
   return (
     <NavigationContainer>
-      <Tab1.Navigator>
 
-        <Tab1.Screen name="Home" component={HomeScreen}
+      <Tab.Navigator screenOptions={({ route }) => {
+        // screenOptions erwartet eine Funktion, diese setzt für jeden Tab.Screen eine Option ein
+        // hier wird mit route überprüft, welcher Tab aktiv ist und setzt mit focused ein outline oder normales icon
+        return {
+          tabBarIcon: ({ focused, size, color }) => {
+            let icon;
+            if (route.name === "Home") { icon = focused ? "home" : "home-outline"; }
+            else if (route.name === "Settings") { icon = focused ? "settings" : "settings-outline"; }
+            return (<Icon.Ionicons name={icon} size={size} color={color} />);
+          },
+          tabBarActiveTintColor: "orange",
+          tabBarStyle: { backgroundColor: "aliceblue" }
+        };
+      }}>
+
+        <Tab.Screen name="Home" component={HomeScreen}
           options={{
-            title: "Freunde", tabBarIcon: ({ focused, size, color }) => {
-              const icon = focused ? "home" : "home-outline";
-              return <Icon.Ionicons name={icon} size={size} color={color} />
-            }
+            title: "Freunde"
           }} />
 
-        <Tab1.Screen name="Settings" component={SettingsScreen}
+        <Tab.Screen name="Settings" component={SettingsScreen}
           options={{
             title: "Einstellungen",
-            tabBarIcon: ({ focused, size, color }) => {
-              const icon = focused ? "settings" : "settings-outline";
-              return <Icon.Ionicons name={icon} size={size} color={color} />
-            }
           }} />
 
-      </Tab1.Navigator>
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
+
+
+
 
